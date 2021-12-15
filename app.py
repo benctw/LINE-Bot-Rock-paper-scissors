42c070e4d43a7d1914de1a6b9880092ab5d92059
Benjamin <benctw@hmail.com>
2021-12-15 22:17:11 +0800

    home page

--- app.py
+++ app.py
@@ -18,6 +18,18 @@ model = load_model('keras_model.h5')
 # create flask server
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+    
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
